631030bcc1dfcbc8babdc3a6a940ce9dba433748
F1cko <filipsabo4@gmail.com>
2025-11-09 13:19:48 +0100

    Update vjezba 5.py

--- vjezba1/vjezba 5.py
+++ vjezba1/vjezba 5.py
@@ -2,6 +2,7 @@
 #1 - nema smisla jer ide od 1 do 2 koji je isklucen pa ispise tako i tako 1
 #2 #ispiše NIŠTA. (petlja se neće ni jednom izvršiti)
 #310, 9, 8, 7, 6, 5, 4, 3, 2
+#vjezba6
 suma = 0
 for i in range(2, 101, 2):
     suma += i
